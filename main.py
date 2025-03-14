cc65da96ec34cd2858213a8398dac7d5f24cb7fb
davinson-ortiz <davinson.ortiz.rivas@gmail.com>
2025-03-14 13:17:00 -0500

    Modify main to print a new massage of usage

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+#! /usr/bin/python3
+
 from stats import count_words, count_characters, sort_characters
 import sys
 
 def main():
     if len(sys.argv) != 2:
-        print("Usage: python3 main.py <path_to_book>")
+        print("Usage: bookbot <path_to_book>")
         sys.exit(1)
 
     file_path = sys.argv[1]
